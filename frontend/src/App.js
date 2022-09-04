e896943949e26aac4dcfb65e0cbd21596e9e046a
Mandeep <mandeep.windsor@gmail.com>
2022-09-03 21:24:42 -0700

    Revised the styling of the app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Button, Modal, Table } from "antd";
+import { Button, Modal, Table, Layout, Typography } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import { AppContext } from "./context/AppContext";
 import { useContext } from "react";
@@ -6,15 +6,12 @@ import { deleteTask } from "./utils/apiUtils";
 import { EditModal } from "./components/EditModal";
 import { AddModal } from "./components/AddModal";
 
+const { Header } = Layout;
+const { Title } = Typography;
+
 const App = () => {
-  const {
-    setTitle,
-    setContent,
-    tasks,
-    setTasks,
-    setIsEditing,
-    setIsAdding,
-  } = useContext(AppContext);
+  const { setTitle, setContent, tasks, setTasks, setIsEditing, setIsAdding } =
+    useContext(AppContext);
 
   const columns = [
     {
@@ -31,6 +28,7 @@ const App = () => {
       key: 3,
       title: "Date",
       dataIndex: "date",
+      sorter: (a, b) => a.date - b.date,
     },
     {
       key: 4,
@@ -69,12 +67,19 @@ const App = () => {
   };
 
   return (
-    <>
-      <Button onClick={() => setIsAdding(true)}>+ Add a new task</Button>
-      <AddModal/>
-      <Table columns={columns} dataSource={tasks} />
-      <EditModal/>
-    </>
+    <Layout>
+      <Header>
+        <Title style={{color: "white", textAlign: "center", paddingTop: "0.5rem"}}>TO DO LIST</Title>
+      </Header>
+      <Layout style={{width: "60%", margin: "5rem auto", gap: "2rem"}}>
+        <Button type="primary" shape="round" onClick={() => setIsAdding(true)}>
+          + Add a new task
+        </Button>
+        <AddModal />
+        <Table columns={columns} dataSource={tasks} />
+        <EditModal />
+      </Layout>
+    </Layout>
   );
 };
 
